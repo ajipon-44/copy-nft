b170e0f597c9e2e433c1ab4d0b5af523c0779e47
ajipon-44 <iyonegaki@gmail.com>
2023-02-18 14:55:31 +0900

    add require msgSender equal to tokenOwner

--- StudentContract.sol
+++ StudentContract.sol
@@ -10,6 +10,7 @@ import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
 
 interface ISchoolContract{
     function tokenURI(uint256 tokenId) external view returns (string memory);
+    function ownerOf(uint256 tokenId) external view returns (address);
 }
 
 contract StudentContract is ERC721URIStorage, Ownable {
@@ -23,11 +24,12 @@ contract StudentContract is ERC721URIStorage, Ownable {
     constructor() ERC721("StudentToken", "StT") {}
 
     // 使用したいコントラクト(Schoolコントラクト)のイーサリアム上のアドレス
-    address SchoolContractAddress = 0x72d9908aB29694287Ad68062a1B32ecED72C67A4;
+    address SchoolContractAddress = 0xf8e81D47203A594245E36C48e151709F0C19fBe8;
     ISchoolContract schoolContract = ISchoolContract(SchoolContractAddress);
 
     /**
      * @dev
+     * - 自分以外のoriginalTokenIdにアクセスしようとした場合はリクエストを拒否する require
      * - defaultで0のトークンIDをインクリメントする _tokenIds.increment()
      * - インクリメントしたトークンIDを変数newTokenIdに入れる
      * - 学生用のMint関数
@@ -37,6 +39,8 @@ contract StudentContract is ERC721URIStorage, Ownable {
      * - 学生のアドレスから企業のアドレスにNFTを送る safeTransferFrom()
     */
     function mintAndTransferToCompany(uint256 originalTokenId, address companyAddress) public onlyOwner {
+        require(ISchoolContract(schoolContract).ownerOf(originalTokenId) == _msgSender(), "caller is not the owner of the token");
+
         _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
 
